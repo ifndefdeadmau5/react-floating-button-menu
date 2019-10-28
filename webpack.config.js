8aaaac9fe20baa1d0ec1563e4340df70a94fdd9b
Trevor Park <trevorme91@gmail.com>
2019-10-28 09:14:18 +0900

    Exclude SC when bundling and handles it as a peerDependency

--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,17 @@ module.exports = {
       commonjs2: 'react',
       amd: 'react',
     },
+    'styled-components': {
+      commonjs: 'styled-components',
+      commonjs2: 'styled-components',
+      amd: 'styled-components',
+    },
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV || 'development',
+      ),
     }),
   ],
 };
-
