3981caacbf087a920a1944034b2a8b6bdb298123
Trevor Park <trevorme91@gmail.com>
2019-12-09 10:09:40 +0900

    Bunch of depndencies update & fix vulnerabilities

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,50 +1,66 @@
-const { resolve } = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  context: resolve(__dirname, 'src'),
+  context: resolve(__dirname, "src"),
 
   entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './index.js',
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://localhost:8080",
+    "webpack/hot/only-dev-server",
+    "./index.js"
   ],
   output: {
-    filename: 'bundle.js',
-    path: resolve(__dirname, 'dist'),
-    publicPath: '/',
+    filename: "bundle.js",
+    path: resolve(__dirname, "dist"),
+    publicPath: "/"
   },
   resolve: {
     alias: {
-      'react-floating-button-menu': resolve(__dirname, '../src'),
-    },
+      "react-floating-button-menu": resolve(__dirname, "../src")
+    }
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
     hot: true,
-    contentBase: resolve(__dirname, 'dist'),
-    publicPath: '/',
+    contentBase: resolve(__dirname, "dist"),
+    publicPath: "/"
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                {
+                  plugins: [
+                    "@babel/plugin-proposal-class-properties",
+                    "@babel/plugin-proposal-export-default-from"
+                  ]
+                }
+              ]
+            }
+          }
+        ],
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader?sourceMap',
-      },
-    ],
+        loader: "style-loader!css-loader?sourceMap"
+      }
+    ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
     new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
-  ],
+      template: "index.html"
+    })
+  ]
 };
